4148e578c66a3233cb6261d052a8e4f59391ac95
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2022-02-09 23:15:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ function compute()
   var years = document.getElementById("years").value;
   var interest = principal * years * rate /100;
   var year = new Date().getFullYear()+parseInt(years);
-  if(principle==0 ||rate==1||years==1)
+  if(principal==-1 ||rate==1||years==1)
   {
      alert("Enter a positive number");
   }
-  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+  var elem=document.getElementById("result");
+  elem.innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+  elem.style.color="yellow";
 
 }
